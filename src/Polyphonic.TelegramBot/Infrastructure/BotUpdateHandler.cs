74682471cebefd4fd059e76d60eda1d4ae9ee552
Tony Skorik <anton.skorik@aliexpress.ru>
2023-08-26 16:56:32 +0400

    Fix compilation;

--- src/Polyphonic.TelegramBot/Infrastructure/BotUpdateHandler.cs
+++ src/Polyphonic.TelegramBot/Infrastructure/BotUpdateHandler.cs
@@ -1,28 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Logging;
 
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace Polyphonic.TelegramBot.Infrastructure;
 internal class BotUpdateHandler : IUpdateHandler
 {
-    public Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    // Store bot screaming status
+    private bool _screaming = false;
+
+    // Pre-assign menu text
+    private const string FIRST_MENU = "<b>Menu 1</b>\n\nA beautiful menu with a shiny inline button.";
+    private const string SECOND_MENU = "<b>Menu 2</b>\n\nA better menu with even more shiny inline buttons.";
+
+    // Pre-assign button text
+    private const string NEXT_BUTTON = "Next";
+    private const string BACK_BUTTON = "Back";
+    private const string TUTORIAL_BUTTON = "Tutorial";
+
+    // Build keyboards
+    private InlineKeyboardMarkup _firstMenuMarkup = new(InlineKeyboardButton.WithCallbackData(NEXT_BUTTON));
+    private InlineKeyboardMarkup _secondMenuMarkup = new(
+        new[] {
+        new[] { InlineKeyboardButton.WithCallbackData(BACK_BUTTON) },
+        new[] { InlineKeyboardButton.WithUrl(TUTORIAL_BUTTON, "https://core.telegram.org/bots/tutorial") }
+        }
+    );
+
+    private ILogger _logger;
+
+    public BotUpdateHandler(ILogger<BotUpdateHandler> logger)
     {
-        await Console.Error.WriteLineAsync(exception.Message);
+        _logger = logger;
     }
 
-    public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+    public Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
     {
-        var user = update.Message?.From;
-        var text = update.Message.Text ?? string.Empty;
+        _logger.LogError(exception.Message);
+
+        return Task.CompletedTask;
+    }
+
+    public async Task HandleUpdateAsync(ITelegramBotClient botCleint, Update update, CancellationToken cancellationToken)
+    {
+        var message = update.Message;
+
+        var user = message?.From;
+        var text = message?.Text ?? string.Empty;
 
         if (user is null)
+        {
             return;
+        }
 
         // Print to console
         Console.WriteLine($"{user.FirstName} wrote {text}");
@@ -30,37 +62,94 @@ internal class BotUpdateHandler : IUpdateHandler
         // When we get a command, we react accordingly
         if (text.StartsWith("/"))
         {
-            await HandleCommand(user.Id, text);
+            await HandleCommand(botCleint, user.Id, text);
         }
-        else if (screaming && text.Length > 0)
+        else if (_screaming && text.Length > 0)
         {
             // To preserve the markdown, we attach entities (bold, italic..)
-            await botClient.SendTextMessageAsync(user.Id, text.ToUpper(), entities: msg.Entities);
+            await botCleint.SendTextMessageAsync(user.Id, text.ToUpper(), entities: message.Entities);
         }
         else
         {   // This is equivalent to forwarding, without the sender's name
-            await botClient.CopyMessageAsync(user.Id, user.Id, msg.MessageId);
+            await botCleint.CopyMessageAsync(user.Id, user.Id, message.MessageId);
         }
 
     }
 
-    async Task HandleCommand(long userId, string command)
+    private async Task HandleCommand(ITelegramBotClient botCleint, long userId, string command)
     {
         switch (command)
         {
             case "/scream":
-                screaming = true;
+                _screaming = true;
                 break;
 
             case "/whisper":
-                screaming = false;
+                _screaming = false;
                 break;
 
             case "/menu":
-                await SendMenu(userId);
+                await SendMenu(botCleint, userId);
                 break;
         }
 
         await Task.CompletedTask;
     }
+
+    // Each time a user interacts with the bot, this method is called
+    private async Task HandleUpdate(ITelegramBotClient botCleint, Update update, CancellationToken cancellationToken)
+    {
+        switch (update.Type)
+        {
+            // A message was received
+            case UpdateType.Message:
+                //await HandleMessage(bot, update.Message!);
+                break;
+
+            // A button was pressed
+            case UpdateType.CallbackQuery:
+                await HandleButton(botCleint, update.CallbackQuery!);
+                break;
+        }
+    }
+
+    private async Task SendMenu(ITelegramBotClient botCleint, long userId)
+    {
+        await botCleint.SendTextMessageAsync(
+            userId,
+            FIRST_MENU,
+            (int)ParseMode.Html,
+            replyMarkup: _firstMenuMarkup
+        );
+    }
+
+    private async Task HandleButton(ITelegramBotClient botCleint, CallbackQuery query)
+    {
+        string text = string.Empty;
+        InlineKeyboardMarkup markup = new(Array.Empty<InlineKeyboardButton>());
+
+        if (query.Data == NEXT_BUTTON)
+        {
+            text = SECOND_MENU;
+            markup = _secondMenuMarkup;
+        }
+        else if (query.Data == BACK_BUTTON)
+        {
+            text = FIRST_MENU;
+            markup = _firstMenuMarkup;
+        }
+
+        // Close the query to end the client-side loading animation
+        await botCleint.AnswerCallbackQueryAsync(query.Id);
+
+        // Replace menu text and keyboard
+        await botCleint.EditMessageTextAsync(
+            query.Message!.Chat.Id,
+            query.Message.MessageId,
+            text,
+            ParseMode.Html,
+            replyMarkup: markup
+        );
+    }
+
 }
--- src/Polyphonic.TelegramBot/PolyphonicTelegramBot.cs
+++ src/Polyphonic.TelegramBot/PolyphonicTelegramBot.cs
@@ -1,41 +1,18 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
 
 using Polyphonic.TelegramBot.Configuration;
-using Polyphonic.TelegramBot.Infrastructure;
 
 using Telegram.Bot;
-using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
-using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Polling;
 
 namespace Polyphonic.TelegramBot;
 
 internal class PolyphonicTelegramBot : IHostedService
 {
-    // Store bot screaming status
-    readonly bool _screaming = false;
-
-    // Pre-assign menu text
-    private const string FIRST_MENU = "<b>Menu 1</b>\n\nA beautiful menu with a shiny inline button.";
-    private const string SECOND_MENU = "<b>Menu 2</b>\n\nA better menu with even more shiny inline buttons.";
-
-    // Pre-assign button text
-    private const string NEXT_BUTTON = "Next";
-    private const string BACK_BUTTON = "Back";
-    private const string TUTORIAL_BUTTON = "Tutorial";
-
     private IServiceProvider _serviceProvider;
     private readonly BotConfiguration _botConfiguration;
-
-    // Build keyboards
-    InlineKeyboardMarkup _firstMenuMarkup = new(InlineKeyboardButton.WithCallbackData(NEXT_BUTTON));
-    InlineKeyboardMarkup _secondMenuMarkup = new(
-        new[] {
-        new[] { InlineKeyboardButton.WithCallbackData(BACK_BUTTON) },
-        new[] { InlineKeyboardButton.WithUrl(TUTORIAL_BUTTON, "https://core.telegram.org/bots/tutorial") }
-        }
-    );
 
     public PolyphonicTelegramBot(
         IServiceProvider serviceProvider,
@@ -55,11 +32,11 @@ internal class PolyphonicTelegramBot : IHostedService
         return Task.CompletedTask;
     }
 
-    private async Task StartBotAsync()
+    private Task StartBotAsync()
     {
         var bot = new TelegramBotClient("<YOUR_BOT_TOKEN_HERE>");
 
-        var handler = new BotUpdateHandler();
+        var handler = _serviceProvider.GetRequiredService<IUpdateHandler>();
 
         using var cts = new CancellationTokenSource();
 
@@ -75,61 +52,7 @@ internal class PolyphonicTelegramBot : IHostedService
 
         // Send cancellation request to stop the bot
         cts.Cancel();
-    }
 
-    // Each time a user interacts with the bot, this method is called
-    async Task HandleUpdate(ITelegramBotClient _, Update update, CancellationToken cancellationToken)
-    {
-        switch (update.Type)
-        {
-            // A message was received
-            case UpdateType.Message:
-                await HandleMessage(update.Message!);
-                break;
-
-            // A button was pressed
-            case UpdateType.CallbackQuery:
-                await HandleButton(update.CallbackQuery!);
-                break;
-        }
-    }
-
-    async Task SendMenu(long userId)
-    {
-        await bot.SendTextMessageAsync(
-            userId,
-            FIRST_MENU,
-            ParseMode.Html,
-            replyMarkup: _firstMenuMarkup
-        );
-    }
-
-    async Task HandleButton(CallbackQuery query)
-    {
-        string text = string.Empty;
-        InlineKeyboardMarkup markup = new(Array.Empty<InlineKeyboardButton>());
-
-        if (query.Data == NEXT_BUTTON)
-        {
-            text = SECOND_MENU;
-            markup = _secondMenuMarkup;
-        }
-        else if (query.Data == BACK_BUTTON)
-        {
-            text = FIRST_MENU;
-            markup = _firstMenuMarkup;
-        }
-
-        // Close the query to end the client-side loading animation
-        await bot.AnswerCallbackQueryAsync(query.Id);
-
-        // Replace menu text and keyboard
-        await bot.EditMessageTextAsync(
-            query.Message!.Chat.Id,
-            query.Message.MessageId,
-            text,
-            ParseMode.Html,
-            replyMarkup: markup
-        );
+        return Task.CompletedTask;
     }
 }
